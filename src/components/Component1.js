c897027e26f992444fc5bdbebb67b7e8f7459ac7
Ro5hi <slen.detablan@gmail.com>
2020-12-21 13:09:41 -0700

    first commit, removed boilerplate

---
+++ src/components/Component1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Component1 extends Component {
+    render() {
+        return (
+            <div className="component1">
+                <p>Hey Hi Hello</p>
+            </div>
+        )
+    }
+}
